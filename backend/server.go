576e08c0726b19a68fc7969ad8c2448e44ba4da9
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-08 17:00:29 +0100

    Host API on port 6969

--- backend/server.go
+++ backend/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"katsu.bio/endpoints"
@@ -8,4 +9,5 @@ import (
 
 func main() {
 	http.HandleFunc("/api/blogs", endpoints.HandleBlogs)
+	log.Fatal(http.ListenAndServe(":6969", nil))
 }
