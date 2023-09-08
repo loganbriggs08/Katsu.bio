132361391f401f5463ddd3df327865f9a4f2fdce
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-08 17:21:49 +0100

    Try to create tables else exit

--- backend/server.go
+++ backend/server.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	"katsu.bio/database"
 	"katsu.bio/endpoints"
+	"log"
+	"net/http"
 )
 
 func main() {
 	if database.Initialize() == true {
-		http.HandleFunc("/api/blogs", endpoints.HandleBlogs)
+		if database.CreateTables() == true {
+			http.HandleFunc("/api/blogs", endpoints.HandleBlogs)
 
-		log.Fatal(http.ListenAndServe(":6969", nil))
+			log.Fatal(http.ListenAndServe(":6969", nil))
+		} else {
+			log.Fatal("Failed to create tables within the database.")
+		}
 	} else {
 		log.Fatal("Database connection could not be established.")
 	}
