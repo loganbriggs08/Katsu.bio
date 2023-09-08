8264947e83e6210b036797a9d66ff7441050c69a
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-08 17:37:41 +0100

    backend -> Backend

--- Backend/database/database.go
+++ Backend/database/database.go
@@ -21,7 +21,7 @@ func Initialize() bool {
 func CreateTables() bool {
 	var tableCreationError error
 
-	_, tableCreationError = database_connection.Exec("CREATE TABLE IF NOT EXISTS blogs(blog_id SMALLINT, blog_title VARCHAR(255), blog_description VARCHAR(255), blog_tags TEXT[]);")
+	_, tableCreationError = database_connection.Exec("CREATE TABLE IF NOT EXISTS blogs(blog_id SMALLINT, blog_title VARCHAR(150), blog_description VARCHAR(200), blog_tag VARCHAR(50));")
 
 	if tableCreationError != nil {
 		return false
---
+++ Backend/go.mod
@@ -0,0 +1,5 @@
+module katsu.bio
+
+go 1.23
+
+require github.com/mattn/go-sqlite3 v1.14.17
