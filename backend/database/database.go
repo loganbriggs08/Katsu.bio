f1eb3ca527fe47cd7c97385b9c2576b8da6d08d2
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-08 17:12:36 +0100

    Establish connection when Initalise is ran

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,5 +1,23 @@
 package database
 
-func initalise() {
-	
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var database_connection *sql.DB
+
+func Initalise() bool {
+	connection, databaseConnectionError := sql.Open("sqlite3", "database.db")
+
+	if databaseConnectionError != nil {
+		return false
+	} else {
+		database_connection = connection
+		return true
+	}
+}
+
+func createTables() bool {
+	database_connection.Exec("CREATE TABLE IF NOT EXISTS ")
 }
