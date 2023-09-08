d32426ea09b8f5fa8efc61275924aff4650e4fe6
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-08 17:40:29 +0100

    Create struct for Blogs

--- Backend/structs/structs.go
+++ Backend/structs/structs.go
@@ -4,3 +4,10 @@ type Error struct {
 	ErrorCode    int64  `json:"error_code"`
 	ErrorMessage string `json:"error_message"`
 }
+
+type Blog struct {
+	BlogID          int64  `json:"blog_id"`
+	BlogTitle       string `json:"blog_title"`
+	BlogDescription string `json:"blog_description"`
+	BlogTag         string `json:"blog_tag"`
+}
