d53d8701a005f471a9382fefc85cee83e1ab037f
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-08 16:58:46 +0100

    Update blogs.go

--- backend/endpoints/blogs.go
+++ backend/endpoints/blogs.go
@@ -1,7 +1,40 @@
 package endpoints
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"katsu.bio/structs"
+)
 
 func HandleBlogs(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		if r.Header.Get("blog-id") == "" {
 
+		} else {
+			fmt.Fprintf(w, "blog-id header was passed.")
+		}
+
+	} else {
+		VideoCallbackError := structs.Error{
+			ErrorCode:    http.StatusMethodNotAllowed,
+			ErrorMessage: "Method used is not accepted at this Endpoint.",
+		}
+
+		VideoCallBackErrorMarshal, MarshalError := json.Marshal(VideoCallbackError)
+
+		if MarshalError != nil {
+			log.Fatal(MarshalError)
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+
+			_, WriteError := w.Write(VideoCallBackErrorMarshal)
+
+			if WriteError != nil {
+				log.Fatal(MarshalError)
+			}
+		}
+	}
 }
