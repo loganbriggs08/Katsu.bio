f60049988c5b4ca4c95d6cf705670d09edfd0344
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-09 14:25:24 +0100

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+
+export default function NotFound() {
+  const router = useRouter()
+  
+  return (
+    useEffect(() => {router.replace("/")})
+  )
+}
