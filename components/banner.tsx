3ba4ddf98772f753208a48ffa12668e2b5370747
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-09 14:52:40 +0100

    Update banner.tsx

--- components/banner.tsx
+++ components/banner.tsx
@@ -4,14 +4,12 @@ import {Card, CardBody, CardFooter} from "@nextui-org/card";
 export const Banner = () => {
 	return (
 		<div style={{  }}>
-			<Card style={{ backgroundColor: "#212121", borderRadius: "6px", width: "40%", marginLeft: "auto", marginRight: "auto", marginTop: "1.5rem" }} shadow="none">
+			<Card style={{ backgroundColor: "#212121", borderRadius: "6px", width: "100%", marginTop: "1.2rem", marginRight: "auto", marginLeft: "auto" }} shadow="none">
 				<CardBody style={{ padding: "0.1rem", marginLeft: "1.5rem", marginRight: "1.5rem" }}>
-					<h1 style={{ fontSize: "2rem", fontWeight: "bold", color: "#FFFFFF" }}>Announcement</h1>
-					<Divider style={{  }}/>
-					<p style={{ fontSize: "1.5rem", color: "#FFFFFF" }}>This website can now be found on Github, </p>
-					
-
-				</CardBody>
+						<h1 style={{ fontSize: "2rem", fontWeight: "bold", color: "#FFFFFF" }}>Announcement</h1>
+						<Divider/>
+						<p style={{ fontSize: "1.5rem", color: "#FFFFFF" }}>This website can now be found on Github, </p>
+					</CardBody>
 			</Card>
 		</div>
 	);
