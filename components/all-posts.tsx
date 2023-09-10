8eb1e056d638d171d7e03a1765cdbc1523c5ebb7
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-10 12:56:42 +0100

    Create all-posts.tsx

---
+++ components/all-posts.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import CustomCard from './customcard';
+
+interface Blog {
+	blog_id: string;
+	blog_title: string;
+	blog_description: string;
+	blog_tag: string;
+}
+
+export const AllPosts = () => {
+    const [blogData, setBlogData] = useState<Blog[]>([]);
+
+	useEffect(() => {
+		fetch("http://localhost:6969/api/blogs")
+			.then((response) => response.json())
+			.then((data) => {
+			setBlogData(data.blog_results);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+	}, []);
+
+	return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "1.5rem" }}>
+        	<div style={{ width: "35%" }}>				
+				{blogData.map((blog) => (
+					<div key={blog.blog_id} style={{ marginBottom: "1rem" }}>
+						<CustomCard blog={blog} />
+					</div>
+				))}
+        	</div>
+    	</div>
+	);
+};
