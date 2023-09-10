149cf5b070797bc3e8fe4eefaea868a319e5b75a
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-10 12:56:56 +0100

    edit posts.tsx a little

--- components/posts.tsx
+++ components/posts.tsx
@@ -1,7 +1,6 @@
-import {Chip} from "@nextui-org/chip";
 import { useEffect, useState } from "react";
-import {Card, CardBody} from "@nextui-org/card";
 import CustomCard from './customcard';
+import { FaArrowRightLong } from 'react-icons/fa6'
 
 interface Blog {
 	blog_id: string;
@@ -26,15 +25,21 @@ export const Posts = () => {
 
 	return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "1.5rem" }}>
-        <div style={{ width: "35%" }}>
-        <h1 style={{ fontSize: "1.8rem", fontWeight: "500", marginRight: "1rem" }}>Recent Posts</h1>
-            
-            {blogData.map((blog) => (
-                <div key={blog.blog_id} style={{ marginBottom: "1rem" }}>
-                    <CustomCard blog={blog}/>
-                </div>
-            ))}
-        </div>
-    </div>
+        	<div style={{ width: "35%" }}>
+				<h1 style={{ fontSize: "1.8rem", fontWeight: "500", marginRight: "1rem" }}>Recent Posts</h1>
+				
+				{blogData.slice(0, 2).map((blog) => (
+					<div key={blog.blog_id} style={{ marginBottom: "1rem" }}>
+						<CustomCard blog={blog} />
+					</div>
+				))}
+
+				<a href="/posts/" style={{ color: "#fff", textDecoration: "none" }}>
+					<p style={{ display: "flex", justifyContent: "flex-end", marginRight: "1rem" }}>
+						All Posts <span style={{ marginLeft: "0.5rem" }}><FaArrowRightLong style={{ marginTop: "auto", marginBottom: "auto" }}/></span>
+					</p>
+				</a>
+        	</div>
+    	</div>
 	);
 };
