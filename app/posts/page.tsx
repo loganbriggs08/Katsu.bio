db91dcfb113136536f255adce12a0b43ec295370
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-10 14:31:51 +0100

    Implement tag system

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,6 +3,7 @@
 import CustomCard from '../../components/customcard';
 import React, { useState, useEffect } from 'react';
 import { Divider } from "@nextui-org/divider";
+import { FaArrowLeftLong } from 'react-icons/fa6'
 
 interface Blog {
   blog_id: string;
@@ -14,6 +15,18 @@ interface Blog {
 const AboutPage: React.FC = () => {
   const [searchValue, setSearchValue] = useState<string>('');
   const [blogResults, setBlogResults] = useState<Blog[]>([]);
+  const [blogTagsData, setBlogTagsData] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6969/api/tags")
+      .then((response) => response.json())
+      .then((data) => {
+        setBlogTagsData(data.tags);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   useEffect(() => {
     let timer: NodeJS.Timeout | null = null;
@@ -78,19 +91,34 @@ const AboutPage: React.FC = () => {
             onChange={(e) => setSearchValue(e.target.value)}
           />
 
-          <Divider />
+        <Divider />
+		<div>
+			{blogTagsData && blogTagsData.length > 0 ? (
+				blogTagsData.map((tag, index) => (
+				<button className='TagButton' onClick={() => setSearchValue(tag)} style={{ marginTop: "0.5rem", marginRight: "0.5rem", padding: "0.5rem", borderRadius: "0.2rem", backgroundColor: "#5865F2", border: "none", color: "#fff" }} key={index}>{tag.toUpperCase()}</button>
+				))
+			) : (
+				<p>No tags available</p>
+			)}
+		</div>
 
           <div>
             {blogResults === null ? (
 				<p style={{ fontSize: "1.3rem" }}>No Results 😔</p>
 			) : blogResults.length === 0 ? (
-				<p style={{ fontSize: "1.3rem" }}>Loading your results.. 😊</p>
+				<p style={{ fontSize: "1.3rem" }}>Loading your results 😊</p>
 			) : (
 				blogResults.map((blog) => (
 					<CustomCard key={blog.blog_id} blog={blog} />
 				  ))
 				)}
           </div>
+
+			<a href="/" style={{ color: "#fff", textDecoration: "none" }}>
+				<p style={{ display: "flex", justifyContent: "flex-end", marginRight: "1rem" }}>
+					<span><FaArrowLeftLong style={{ marginTop: "auto", marginBottom: "auto", marginRight: "0.5rem" }}/>Go Back</span>
+				</p>
+			</a>
         </div>
       </div>
     </div>
