893c5620a2d82b23d01571e900736201ee2c32d4
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-10 15:04:48 +0100

    Update not-found to serve basic html

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,31 @@
 'use client'
 
-import { useEffect } from "react"
-import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
-  const router = useRouter()
-  
+  const router = useRouter();
+  const currentUrl = window.location.href;
+  const isPostsPage = currentUrl.includes("/posts/");
+
+  const [userHtmlContent, setUserHtmlContent] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (isPostsPage) {
+      const userInput = "<h1>Hello dsd</h1>";
+
+      setUserHtmlContent(userInput);
+    } else {
+      router.replace("/");
+    }
+  }, [isPostsPage, router]);
+
   return (
-    useEffect(() => {router.replace("/")})
-  )
+    <div>
+      {userHtmlContent && (
+        <div dangerouslySetInnerHTML={{ __html: userHtmlContent }} />
+      )}
+    </div>
+  );
 }
+
