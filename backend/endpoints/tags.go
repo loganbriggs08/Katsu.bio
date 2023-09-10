01fa7af7b9d46d5cc0289016258b217ec2058939
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-10 13:30:31 +0100

    Create and finish HandleTags function

--- backend/endpoints/tags.go
+++ backend/endpoints/tags.go
@@ -1,7 +1,34 @@
 package endpoints
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"katsu.bio/structs"
+)
 
 func HandleTags(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		fmt.Println("Correct method")
+	} else {
+		MethodError := structs.Error{
+			ErrorCode:    http.StatusMethodNotAllowed,
+			ErrorMessage: "Method used is not accepted at this Endpoint.",
+		}
 
+		MethodErrorMarshal, MethodErrorMarshalError := json.Marshal(MethodError)
+
+		if MethodErrorMarshalError != nil {
+			log.Fatalln(MethodErrorMarshalError)
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			_, writeResponseError := w.Write(MethodErrorMarshal)
+
+			if writeResponseError != nil {
+				log.Fatal(writeResponseError)
+			}
+		}
+	}
 }
