c3d857cb64254609e2e84760fb8093c40d36585b
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-10 12:56:38 +0100

    Create /posts

---
+++ app/posts/page.tsx
@@ -0,0 +1,100 @@
+'use client'
+
+import CustomCard from '../../components/customcard';
+import React, { useState, useEffect } from 'react';
+import { Divider } from "@nextui-org/divider";
+
+interface Blog {
+  blog_id: string;
+  blog_title: string;
+  blog_description: string;
+  blog_tag: string;
+}
+
+const AboutPage: React.FC = () => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [blogResults, setBlogResults] = useState<Blog[]>([]);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout | null = null;
+
+    const fetchBlogResults = async () => {
+      try {
+        const headers = new Headers();
+        headers.append('query', searchValue);
+
+        const response = await fetch('http://localhost:6969/api/blogs', {
+          method: 'GET',
+          headers: headers,
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        setBlogResults(data.blog_results);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(() => {
+        fetchBlogResults();
+    }, 500);
+
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [searchValue]);
+
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "8%" }}>
+        <div style={{ width: "35%" }}>
+          <h1 style={{ fontSize: "40px", fontWeight: "500", marginBottom: "1rem" }}>All Posts</h1>
+
+          <input
+            className="SearchPosts"
+            placeholder="Search Posts"
+            style={{
+              backgroundColor: "#292932",
+              paddingLeft: "0.8rem",
+              marginBottom: "1rem",
+              border: "none",
+              color: "#fff",
+              width: "60%",
+              height: "3rem",
+              borderRadius: "0.2rem",
+              fontSize: "large"
+            }}
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+
+          <Divider />
+
+          <div>
+            {blogResults === null ? (
+				<h1>No Results 1</h1>
+			) : blogResults.length === 0 ? (
+				<h1>No results</h1>
+			) : (
+				blogResults.map((blog) => (
+					<CustomCard key={blog.blog_id} blog={blog} />
+				  ))
+				)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutPage;
