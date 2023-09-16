f16809f4767dc0a2b1d5dcd6a100a8a424f6c85d
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-09-16 13:44:01 +0100

    Create RandomString function

--- backend/modules/random/string.go
+++ backend/modules/random/string.go
@@ -1,1 +1,21 @@
 package modules
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	characters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	var result string
+
+	for i := 0; i < length; i++ {
+		index := rand.Intn(len(characters))
+
+		result += string(characters[index])
+	}
+
+	return result
+}
